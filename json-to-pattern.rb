511bc53849542c09100b346eb6f590274ec9ea55
Joao Peixoto <joao.hartimer@gmail.com>
2017-02-05 11:42:27 -0800

    Use correct sha256

--- json-to-pattern.rb
+++ json-to-pattern.rb
@@ -2,7 +2,7 @@ class JsonToPattern < Formula
   desc "Applies a JSON object to a String pattern"
   homepage "https://github.com/Hartimer/json-to-pattern"
   url "https://github.com/Hartimer/json-to-pattern/archive/1.0.0.tar.gz"
-  sha256 "789de4bffe0c22911a4968e525feeb20a6c7c4f4fe762a936ce2dac2213cd855"
+  sha256 "674d6ffbb3e57e2c0c47c3712d9034247ef3710d82fc66c4e9f0d012cba6108b"
 
   include Language::Python::Virtualenv
 
