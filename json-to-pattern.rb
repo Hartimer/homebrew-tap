3a4c94a3733c18e88fbef3a34a2e62f499b866b7
Hartimer <joao.hartimer@gmail.com>
2017-09-22 10:31:24 -0700

    Update library version

--- json-to-pattern.rb
+++ json-to-pattern.rb
@@ -1,8 +1,8 @@
 class JsonToPattern < Formula
   desc "Applies a JSON object to a String pattern"
   homepage "https://github.com/Hartimer/json-to-pattern"
-  url "https://github.com/Hartimer/json-to-pattern/archive/1.0.0.tar.gz"
-  sha256 "674d6ffbb3e57e2c0c47c3712d9034247ef3710d82fc66c4e9f0d012cba6108b"
+  url "https://github.com/Hartimer/json-to-pattern/archive/1.0.1.tar.gz"
+  sha256 "b5aa01ab968fdee6a9193f1f3ecc7ad9352535367bc49ea062a63510962bbc9f"
 
   include Language::Python::Virtualenv
 
